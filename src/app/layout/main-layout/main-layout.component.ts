b8f207b22155dc1127369af0c1df9c9fe83f726d
abdul-naufal <abdul@getrealhealth.com>
2025-11-28 14:00:50 +0530

    updated hamburger

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
+  pageTitle: string = 'DASHBOARD';
+  sectionTitle: string = 'OPS';
 
+  private routeTitles: { [key: string]: string } = {
+    '/dashboard': 'COCKPIT',
+    '/generate-timetable': 'COURSE TIMETABLE',
+    '/manage-syllabus': 'TRAINING SYLLABI',
+    '/question-papers': 'EXAM PAPERS',
+    '/digital-certificates': 'CERTIFICATES',
+    '/question-papers/create': 'CREATE EXAM PAPER',
+    '/question-papers/design': 'DESIGN EXAM PAPER'
+  };
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updateTitle();
+    });
+
+    // Initial update
+    this.updateTitle();
+  }
+
+  private updateTitle() {
+    const currentUrl = this.router.url.split('?')[0]; // Ignore query params
+
+    // Handle dynamic routes (e.g., /manage-syllabus/1)
+    if (currentUrl.startsWith('/manage-syllabus/')) {
+      this.pageTitle = 'SYLLABUS DETAILS';
+    } else if (this.routeTitles[currentUrl]) {
+      this.pageTitle = this.routeTitles[currentUrl];
+    } else {
+      this.pageTitle = 'DASHBOARD'; // Default
+    }
+  }
 }
